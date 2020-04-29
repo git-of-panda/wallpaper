c3ece357dc7411296113e8a2ebda7644c707f55c
Darren Stokes <darren.stokes@rentalmatics.com>
2020-04-29 16:40:54 +0100

    Added functions to get list of Imaginary subreddits

--- wallpaper.py
+++ wallpaper.py
@@ -1,4 +1,6 @@
 import os
+from bs4 import BeautifulSoup
+import requests
 
 def config_check():
     # get current directory so we can use absolute paths
@@ -41,7 +43,7 @@ def file_location(picture_location_root, picture_location_dir):
                 or create_confirm == "" or create_confirm == "Yes":
                     os.mkdir(picture_location_dir)
                 else:
-                    
+                    pass
         # Gibberish was entered, reprompt:
         else:
             file_location(picture_location_root, picture_location_dir)
@@ -52,6 +54,32 @@ def file_location(picture_location_root, picture_location_dir):
     #with open(config_file, "w") as con_fi:
     #    con_fi.write("picture_folder: %s" % picture_location_dir)
 
+def get_html():
+    # Spoof connection as Firefox in order to get back what we want
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64; rv:77.0) Gecko/20100101 Firefox/77.0'}
+    url = "https://www.reddit.com/r/ImaginaryNetwork/wiki/networksublist"
+    html_page = requests.get(url, headers=headers)
+    if html_page.status_code == 200:
+        imaginary_links = get_links(html_page)
+        return imaginary_links
+    else:
+        print("Unsuccessful in getting the HTML. HTTP response: %s" % html_page.status_code)
+        exit(1)
+    
+def get_links(html_page):
+    # Convert HTML and search for all hyperlinks
+    reddit_soup = BeautifulSoup(html_page.content, 'html.parser')
+    links = reddit_soup.find_all('a', href=True)
+    
+    # Create list to hold links
+    imaginary_links = []
+
+    # Loop through and add any with /r/ into the list
+    for l in links:
+        if "/r/" in l.text:
+            imaginary_links.append(l.text)
+    print(imaginary_links)
+    return imaginary_links
 
 if __name__ == "__main__":
     # set global variables
@@ -60,4 +88,5 @@ if __name__ == "__main__":
         domain = output.split("\\")[0].strip()
         user = output.split("\\")[1].strip()
     
-    config_check()
+    #config_check()
+    get_html()
