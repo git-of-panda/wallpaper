e72baf71afc6f7b0bb7e631a3d10047ad7cad3b6
git-of-panda <darrenstokes2@hotmail.com>
2020-05-05 22:08:30 +0100

    Enabled config parsing for existing config files

--- wallpaper.py
+++ wallpaper.py
@@ -2,6 +2,8 @@ import os
 from bs4 import BeautifulSoup
 import requests
 import subprocess
+import re
+import random
 
 def config_check():
     # get current directory so we can use absolute paths
@@ -11,8 +13,59 @@ def config_check():
     # check for the config file
     if os.path.exists(config_file):
         print("Config file exists")
+        config_parse(config_file)
     else:
-        create_config( config_file)
+        create_config(config_file)
+
+def config_parse(config_file):
+    cf = open(config_file, 'r')
+
+    # use bool to track if we're in the subreddit section
+    subreddit_bool = False
+    subreddit_list = []
+
+    # go through config file
+    for line in cf.readlines():
+        # get list of subreddits
+        if "Subreddit List" in line:
+            subreddit_bool = True
+        if subreddit_bool is True:
+            subreddit = re.findall(r'"(.*?)"', line)
+            subreddit_list = subreddit_list + subreddit
+        elif "]":
+            subreddit_bool = False
+        
+        # get download location
+        if "Download Directory" in line:
+            download_directory = line.split()[-1]
+        
+        # Number of monitors
+        if "Number of monitors" in line:
+            no_of_monitors = int(line.split()[-1])
+        
+        # different wallpaper per monitor
+        if "Unique wallpaper per monitor" in line:
+            unique_per_monitor = line.split()[-1]
+    
+    if unique_per_monitor:
+        chosen_subs = pick_subs(subreddit_list, no_of_monitors)
+    else:
+        chosen_subs = random.choice(subreddit_list)
+    
+    get_picture_url(chosen_subs, no_of_monitors)
+
+
+def get_picture_url(chosen_subs, no_of_monitors):
+    # get the link of the top picture of the week
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64; rv:77.0) Gecko/20100101 Firefox/77.0'}
+
+    for subreddit in chosen_subs:
+        url = "https://www.reddit.com/r/%s/top/?sort=top&t=week" % subreddit
+
+
+def pick_subs(subreddit_list, no_of_monitors):
+    chosen_subs = random.sample(subreddit_list, k=no_of_monitors)
+    return chosen_subs
 
 def create_config(config_file):
     print("File not found, assuming first run, creating new config file: \"%s\"" % config_file)
@@ -23,16 +76,20 @@ def create_config(config_file):
 
     # Get download directory from user
     download_path = wallpaper_directory(picture_location_root, picture_location_dir)
-    list_of_subreddits = get_html()
+    urls = ["https://www.reddit.com/r/ImaginaryNetwork/wiki/networksublist", "https://www.reddit.com/r/sfwpornnetwork/wiki/network"]
+    list_of_subreddits = get_html(urls)
     monitors, different_wallpapers = numbers_monitors()
     
+    # Write everything to config file
     print("Writing to config file")
     with open(config_file, 'w') as cf:
         cf.write("Download Directory: %s\n" % download_path)
         cf.write("Number of monitors: %s\n" % monitors)
         cf.write("Unique wallpaper per monitor: %s\n" % different_wallpapers)
-        cf.write("Subreddit List: [" % list_of_subreddits)
-        for i,sub in enumerate(list_of_subreddits):
+        
+        cf.write("Subreddit List: [")
+        # use enumerate to be able to format the list
+        for i,sub in enumerate(sorted(list_of_subreddits)):
             if i == 0:
                 cf.write("\"%s\",\n" % sub)
             elif i != len(list_of_subreddits) - 1:
@@ -54,8 +111,10 @@ def wallpaper_directory(picture_location_root, picture_location_dir):
         elif location_confirm == "N" or location_confirm == "No":
             picture_location_dir = input("Please enter fullpath for wallpaper storage: ")
 
+            # Use backslash escaping for Python
             picture_location_dir = picture_location_dir.replace("\\", "\\\\")
             print("Checking supplied location %s" % picture_location_dir)
+
             # Check directory exists
             if not os.path.exists(picture_location_dir):
                 create_confirm = input("%s doesn't exist, create? Y/n")
@@ -73,16 +132,21 @@ def wallpaper_directory(picture_location_root, picture_location_dir):
     else:
         print("Default Pictures folder doesn't exist")
         picture_location_dir = input( "Please enter fullpath for wallpaper storage:")
-    
+
     return picture_location_dir
 
-def get_html():
+def get_html(urls):
     # Spoof connection as Firefox in order to get back what we want
     headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64; rv:77.0) Gecko/20100101 Firefox/77.0'}
-    urls = ["https://www.reddit.com/r/ImaginaryNetwork/wiki/networksublist", "https://www.reddit.com/r/sfwpornnetwork/wiki/network"]
+    
+    # list to hold URLs
     url_links = []
+    
+    # query both URLs
     for url in urls:
+        # get the HTML of the page
         html_page = requests.get(url, headers=headers)
+        # 200 = web page request was successful
         if html_page.status_code == 200:
             subreddit_links = get_links(html_page)
             url_links = url_links + subreddit_links
@@ -132,4 +196,3 @@ if __name__ == "__main__":
         user = output.split("\\")[1].strip()
     
     config_check()
-    get_html()
